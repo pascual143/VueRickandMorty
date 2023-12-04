5a6301e0dfd02385d549e3578e940302897d6839
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-12-04 15:13:58 +0100

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp, Vue } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+
+createApp(App).use(store).use(router).use(VueAxios, axios).mount('#app')
